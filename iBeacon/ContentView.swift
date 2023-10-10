690279d3a4c7d5f49b6240f48062bdbce00cc8c0
Mehdi Parsaei <mehdiparsaeigm@gmail.com>
2023-10-10 16:55:25 +0200

    initial commit

--- iBeacon/ContentView.swift
+++ iBeacon/ContentView.swift
@@ -6,16 +6,27 @@
 //
 
 import SwiftUI
+import CoreLocation
+import CoreBluetooth
 
 struct ContentView: View {
+    @State private var locationManager = CLLocationManager()
+    @State private var centralManager = CBCentralManager()
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+            Text("Hello, world!").onAppear{
+                // Request always-on location access
+                               locationManager.requestAlwaysAuthorization()
+                
+                // Request Bluetooth permission
+                              centralManager = CBCentralManager(delegate: nil, queue: nil, options: [CBCentralManagerOptionShowPowerAlertKey: true])
+                         
+            }
+        }.padding()
     }
 }
 
--- iBeacon/iBeaconApp.swift
+++ iBeacon/iBeaconApp.swift
@@ -6,12 +6,45 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct iBeaconApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+        
+    init() {
+        FirebaseApp.configure()
+
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    var databaseReference: DatabaseReference = Database.database().reference(fromURL: "https://ibeacon-dde3e-default-rtdb.firebaseio.com/")
+    
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        self.databaseReference.child(formattedTime() + " will Finish Launching WithOptions").setValue(0)
+        return true
+    }
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        self.databaseReference.child(formattedTime() + "  did Finish Launching WithOptions").setValue(0)
+        return true
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        self.databaseReference.child(formattedTime() + " application Will Terminate").setValue(0)
+    }
+    
+    func formattedTime() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss SSS"
+            
+        return dateFormatter.string(from: Date())
     }
 }
