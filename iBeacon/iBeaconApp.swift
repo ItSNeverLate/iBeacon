8d0b9a00ea0c036bdf1567866ec9926e091463f6
Mehdi Parsaei <mehdiparsaeigm@gmail.com>
2023-10-10 11:32:59 +0200

    Initial Commit

---
+++ iBeacon/iBeaconApp.swift
@@ -0,0 +1,17 @@
+//
+//  iBeaconApp.swift
+//  iBeacon
+//
+//  Created by Parsa on 10.10.23.
+//
+
+import SwiftUI
+
+@main
+struct iBeaconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
